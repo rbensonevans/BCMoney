8c4ca56de1a84d22797a038e4f763ffa5016a47d
R. Benson Evans <r.benson.evans@gmail.com>
2026-02-05 06:15:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8602180992-d5d67",
+  "appId": "1:827785500686:web:e9a249a44f999f3ed9c45f",
+  "apiKey": "AIzaSyAbdbB3aMYA2Yk40t0b56tvtqARre3AxIA",
+  "authDomain": "studio-8602180992-d5d67.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "827785500686"
+};
