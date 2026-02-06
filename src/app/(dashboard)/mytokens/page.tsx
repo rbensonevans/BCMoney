bfa49f37689ff9a2b58580ed31b58612b85c7542
R. Benson Evans <r.benson.evans@gmail.com>
2026-02-06 23:52:48 +0000

    Replace in dashboard and mytokens the stylized Kaspa icon placeholder wi

--- src/app/(dashboard)/mytokens/page.tsx
+++ src/app/(dashboard)/mytokens/page.tsx
@@ -266,7 +266,7 @@ function TokenIcon({ token, size = 32 }: { token: any, size?: number }) {
     return (
       <div className="rounded-full overflow-hidden shrink-0" style={{ width: size, height: size }}>
         <Image 
-          src="https://s2.coinmarketcap.com/static/img/coins/64x64/20396.png" 
+          src="https://s2.coinmarketcap.com/static/img/coins/64x64/512.png" 
           alt="KAS" 
           width={size} 
           height={size} 
